61ac080195dd2debe17a5829de98f60b7d0c8500
bryannaegele <bryan@fourthtime.com>
2020-11-08 20:28:11 -0700

    v0.1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OpentelemetryPhoenix.MixProject do
     [
       app: :opentelemetry_phoenix,
       description: description(),
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
